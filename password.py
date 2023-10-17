8858819c6eb3a069bb0076fd89d02e7705dffe75
mquiroga42 <117262918+mquiroga42@users.noreply.github.com>
2023-10-17 07:07:53 +0000

    checkpoint

--- password.py
+++ password.py
@@ -28,13 +28,44 @@ class PasswordConfig:
         self.numbers = numbers
         self.symbols = symbols
 
-def password_config_menu(config):
+config = PasswordConfig()
+
+def password_config_menu():
     """
     Menu for configuring the password.
     """
-    
+    while True:
+        os.system("clear")
+        print(pyfiglet.figlet_format("Password Configurator", font="big"))
+        # Menu Options
+        options = [ "[1] SetLengthPassword (" + str(config.length) + ")",
+                    "[2] UpperCase (" + str(config.upper_case) + ")",
+                    "[3] LowerCase (" + str(config.lower_case) + ")",
+                    "[4] Numbers (" + str(config.numbers) + ")",
+                    "[5] Symbols (" + str(config.symbols) + ")",
+                    "[6] Back"]
+        terminal_menu = TerminalMenu(options)
+        menu_entry_index = terminal_menu.show()
+        # Menu Actions
+        if menu_entry_index == 0:
+            while True:
+                try:
+                    config.length = int(input("Length of the password: "))
+                    break
+                except ValueError:
+                    print("Invalid input")
+        if menu_entry_index == 1:
+            config.upper_case = not config.upper_case
+        if menu_entry_index == 2:
+            config.lower_case = not config.lower_case
+        if menu_entry_index == 3:
+            config.numbers = not config.numbers
+        if menu_entry_index == 4:
+            config.symbols = not config.symbols
+        if menu_entry_index == 5:
+            return config
 
-def generate_password(config):
+def generate_password():
     """
     Generate a random password.
 
@@ -71,13 +102,15 @@ def generate_password_list(main_menu):
         os.system("clear")
         print(pyfiglet.figlet_format("Dictionary Generator", font="big"))
         # Menu Options
-        options = ["[1] Generate Dictionary", "[2] Dictionary List Size (" + str(size) + ")", "[3] Password Configuration", "[4] Back"]
+        options = [ "[1] Generate Dictionary",
+                    "[2] Dictionary List Size (" + str(size) + ")",
+                    "[3] Password Configuration",
+                    "[4] Back"]
         terminal_menu = TerminalMenu(options)
         menu_entry_index = terminal_menu.show()
         # Menu Actions
         if menu_entry_index == 0:
-            config = PasswordConfig()
-            word_list = [generate_password(config) for _ in range(size)]
+            word_list = [generate_password() for _ in range(size)]
             download_dictionary(word_list, "test")
             break
         if menu_entry_index == 1:
@@ -88,6 +121,6 @@ def generate_password_list(main_menu):
                 except ValueError:
                     print("Invalid input")
         if menu_entry_index == 2:
-            continue
+            config = password_config_menu()
         if menu_entry_index == 3:
             return
