c8486232082d7e5497af2695493f3e3f8e9c4006
mquiroga42 <117262918+mquiroga42@users.noreply.github.com>
2023-10-24 07:15:52 +0000

    mejora firewall

--- Firewall simple/main.py
+++ Firewall simple/main.py
@@ -20,14 +20,36 @@ def handle_packet(packet):
     if IP in packet and packet[IP].src in blocked_list:
         src_ip = packet[IP].src
         packet_type = packet[IP].proto
-        if packet_type == 1:
-            packet_type = "ICMP"
-        elif packet_type == 6:
-            packet_type = "TCP"
-        elif packet_type == 17:
-            packet_type = "UDP"
         if packet_type in blocked_list[src_ip]:
             print("Blocked packet from " + src_ip + " of type " + packet_type)
+            return
+
+def configure_firewall():
+    """
+    Function that configures the firewall.
+    """
+    while True:
+        os.system("clear")
+        print(pyfiglet.figlet_format("Configure Firewall", font="big", justify="center"))
+        # Menu Options
+        options = []
+        for i, (ip, protocols) in enumerate(blocked_list.items(), start=1):
+            formatted_protocols = " - ".join(protocols)
+            options.append(f"[{i}] {ip}: {formatted_protocols}")
+
+        options.append(f"[{len(blocked_list) + 1}] Add new IP to Block List")
+        options.append(f"[{len(blocked_list) + 2}] Delete IP from Block List")
+        options.append(f"[{len(blocked_list) + 3}] Back")
+        terminal_menu = TerminalMenu(options)
+        menu_entry_index = terminal_menu.show()
+        # Menu Actions
+        if len(blocked_list) > menu_entry_index:
+            print("Edit IP")
+        elif menu_entry_index == len(blocked_list):
+            print("Remove IP from Block List")
+        elif menu_entry_index == len(blocked_list) + 1:
+            print("Remove Protocol from Block List")
+        if menu_entry_index == len(blocked_list) + 2:
             return
 
 def main_menu():
@@ -45,7 +67,7 @@ def main_menu():
         menu_entry_index = terminal_menu.show()
         # Menu Actions
         if menu_entry_index == 0:
-            print("Configure Firewall")
+            configure_firewall()
         elif menu_entry_index == 1:
             sniff(prn=handle_packet)
         if menu_entry_index == 2:
