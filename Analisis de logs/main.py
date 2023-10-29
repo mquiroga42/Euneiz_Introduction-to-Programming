7461144d0e45c10a5a7e255e41da1d9fdd3115e3
mquiroga42 <117262918+mquiroga42@users.noreply.github.com>
2023-10-29 15:23:31 +0000

    OK

--- Analisis de logs/main.py
+++ Analisis de logs/main.py
@@ -1,3 +1,7 @@
+"""
+Log Analytics
+"""
+
 import os
 import re
 import time
@@ -5,92 +9,92 @@ from datetime import datetime, timedelta
 import pyfiglet
 from simple_term_menu import TerminalMenu
 
-# Función para detectar ataques de fuerza bruta en registros de Apache
 def detect_apache_bruteforce(logfile):
+    """
+    Function to detect brute force attacks in Apache logs.
+    Args:
+    logfile (str): The path to the Apache log file.
+    Returns:
+    list: A list of IP addresses that have been detected as brute force attackers.
+    """
     failed_attempts = {}
     alert_threshold = 10
-    time_window = 5  # Cambiado a minutos
+    time_window = 5
     ip_alerted = set()
-
-    brute_force_details = []  # Lista para almacenar detalles de ataques de fuerza bruta
-
-    # Leer el archivo de registro
-    with open(logfile, "r") as log:
+    brute_force_details = []
+    with open(logfile, "r", encoding="UTF-8") as log:
         for line in log:
             if "GET /login.html" in line and " 401 " in line:
                 parts = line.split()
-                timestamp = parts[3] + " " + parts[4]  # Tomar la fecha y hora
-
-                # Extraer la hora de la cadena
+                timestamp = parts[3] + " " + parts[4]
                 log_time = datetime.strptime(timestamp, "[%d/%b/%Y:%H:%M:%S +%f]")
                 ip = parts[0]
-
                 if ip in failed_attempts:
                     if (log_time - failed_attempts[ip][-1]) <= timedelta(minutes=time_window):
                         failed_attempts[ip].append(log_time)
                         if len(failed_attempts[ip]) >= alert_threshold and ip not in ip_alerted:
                             ip_alerted.add(ip)
-                            brute_force_details.append(ip)  # Almacenar detalles de ataques
+                            brute_force_details.append(ip)
                     else:
                         failed_attempts[ip] = [log_time]
                 else:
                     failed_attempts[ip] = [log_time]
+    return brute_force_details
 
-    return brute_force_details  # Devolver detalles de ataques de fuerza bruta en Apache
-
-# Función para detectar ataques de fuerza bruta en registros de SSH
 def detect_bruteforce_ssh(logfile):
+    """
+    Function to detect brute force attacks in SSH logs.
+    Args:
+    logfile (str): The path to the SSH log file.
+    Returns:
+    list: A list of tuples containing the IP address and the user that have been detected as brute force attackers.
+    """
     failed_attempts = {}
     alert_threshold = 10
-    time_window = 5  # Cambiado a minutos
+    time_window = 5
     ip_alerted = set()
-
-    brute_force_details = []  # Lista para almacenar detalles de ataques de fuerza bruta
-
-    # Leer el archivo de registro
-    with open(logfile, "r") as log:
+    brute_force_details = []
+    with open(logfile, "r", encoding="UTF-8") as log:
         for line in log:
             if "sshd" in line and "Failed password" in line:
                 parts = line.split()
-                timestamp = parts[0] + " " + parts[1] + " " + parts[2]  # Tomar solo el mes, día y hora
-
-                # Extraer la hora de la cadena
+                timestamp = parts[0] + " " + parts[1] + " " + parts[2]
                 log_time = datetime.strptime(timestamp, "%b %d %H:%M:%S")
                 ip = re.search(r'\d+\.\d+\.\d+\.\d+', line)
                 if ip:
                     ip = ip.group()
                 else:
-                    continue  # Ignorar líneas sin dirección IP
-
+                    continue
                 user = None
-                # Buscar el usuario
                 user_match = re.search(r'for (\w+) from', line)
                 if user_match:
                     user = user_match.group(1)
-
                 if ip in failed_attempts:
                     if (log_time - failed_attempts[ip][-1]) <= timedelta(minutes=time_window):
                         failed_attempts[ip].append(log_time)
                         if len(failed_attempts[ip]) >= alert_threshold and ip not in ip_alerted:
                             ip_alerted.add(ip)
-                            brute_force_details.append((ip, user))  # Almacenar detalles de ataques
+                            brute_force_details.append((ip, user))
                     else:
                         failed_attempts[ip] = [log_time]
                 else:
                     failed_attempts[ip] = [log_time]
+    return brute_force_details 
 
-    return brute_force_details  # Devolver detalles de ataques de fuerza bruta
-
-# Función para mostrar un menú de detección de ataques de fuerza bruta
 def detect_brute_force_menu():
+    """
+    Function that displays the submenu to detect brute force attacks.
+    Args:
+    None
+    Returns:
+    None
+    """
     while True:
         os.system("clear")
         print(pyfiglet.figlet_format("Detect Brute Force", font="big", justify="center"))
-        # Submenu Options
         options = ["[1] SSH", "[2] Apache", "[3] Back to Main Menu"]
         submenu = TerminalMenu(options)
         submenu_entry_index = submenu.show()
-
         if submenu_entry_index == 0:
             logfile = "/workspaces/Euneiz_Introduction-to-Programming/Analisis de logs/auth.log"  # Ruta al archivo de registro de SSH, ajusta según tu sistema
             print("Detecting Brute Force for SSH...")
@@ -114,23 +118,33 @@ def detect_brute_force_menu():
         elif submenu_entry_index == 2:
             break
 
-# Función para mostrar el menú principal
 def main_menu():
+    """
+    Function that displays the program's main menu.
+    Args:
+    None
+    Returns:
+    None
+    """
     while True:
         os.system("clear")
         print(pyfiglet.figlet_format("Menu", font="big", justify="center"))
-        # Main Menu Options
         options = ["[1] Detect Brute Force", "[2] Exit"]
         main_menu = TerminalMenu(options)
         main_menu_entry_index = main_menu.show()
-
         if main_menu_entry_index == 0:
             detect_brute_force_menu()
         elif main_menu_entry_index == 1:
             return
 
-# Función principal
 def main():
+    """
+    Main function of the program.
+    Args:
+    None
+    Returns:
+    None
+    """
     os.system("clear")
     print(pyfiglet.figlet_format("Euneiz", font="big", justify="center"))
     time.sleep(0.5)
--- Codificador y Decodificador base64/main.py
+++ Codificador y Decodificador base64/main.py
@@ -1,37 +1,57 @@
+"""
+Main file of the program.
+"""
+
 import base64
 import os
 import time
 import pyfiglet
 from simple_term_menu import TerminalMenu
 
-# Función para decodificar una cadena en base64
 def decode_base64(string_codificado):
+    """
+    Function to decode a string in base64.
+    Args:
+    string_codificado (str): The string to decode.
+    Returns:
+    str: The decoded string.
+    """
     try:
         string_decodificado = base64.b64decode(string_codificado).decode('utf-8')
         return string_decodificado
     except Exception as e:
         return f"Error: {e}"
 
-# Función para codificar una cadena en base64
 def encode_base64(string_decodificado):
+    """
+    Function to encode a string in base64.
+    Args:
+    string_decodificado (str): The string to encode.
+    Returns:
+    str: The encoded string.
+    """
     try:
         string_codificado = base64.b64encode(string_decodificado.encode('utf-8')).decode('utf-8')
         return string_codificado
     except Exception as e:
         return f"Error: {e}"
 
-# Función para mostrar el menú de opciones
 def menu():
+    """
+    Function that displays the program's main menu.
+    Args:
+    None
+    Returns:
+    None
+    """
     string_decodificado = ""
     string_codificado = ""
-    
     while True:
         os.system("clear")
         print(pyfiglet.figlet_format("Menu", font="big", justify="center"))
         options = ["[1] Decode string in base64", "[2] Encode string in base64", "[3] Exit"]
         terminal_menu = TerminalMenu(options)
         menu_entry_index = terminal_menu.show()
-
         if menu_entry_index == 0:
             string_codificado = input("Add the string to decode: ")
             if string_codificado:
@@ -41,7 +61,6 @@ def menu():
             else:
                 print("You must provide a string to decode")
                 time.sleep(1)
-
         elif menu_entry_index == 1:
             string_decodificado = input("Add the string to encode: ")
             if string_decodificado:
@@ -51,12 +70,17 @@ def menu():
             else:
                 print("You must provide a string to encode")
                 time.sleep(1)
-
         elif menu_entry_index == 2:
             break
 
-# Función principal
 def main():
+    """
+    Main function of the program.
+    Args:
+    None
+    Returns:
+    None
+    """
     os.system("clear")
     print(pyfiglet.figlet_format("Euneiz", font="big", justify="center", width=100))
     time.sleep(0.5)
